cd1c7f48793aa428f04dca84fb1f22b5bf196da4
webpropluss <amantedelafelicidad20@gmail.com>
2026-08-17 21:17:07 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -6,8 +6,8 @@
 //    - anon public key  → SUPABASE_ANON_KEY
 // ============================================================
 
-const SUPABASE_URL = 'https://eaqwrpjhjpjsjogvdfbq.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_s1qRrwsElOJVxVr30nHSnA_QWVTSjLB';
+const SUPABASE_URL = 'https://gmhotbwtkzaldugxmqjr.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_YvDeikAP3XpF6Rsro-XXrA_52kQnMZB';
 
 // Cliente global (la librería se carga por CDN en cada página)
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
